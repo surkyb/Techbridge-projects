46e4199bfaff4a732028c397005e4e918dc0fa01
JoshuaMan230 <Joshuasat001@gmail.com>
2026-02-03 16:23:47 -0400

    Add files via upload
    Asignacion 1 / 2025-1107 Joshua Abreu

---
+++ 2025-1107 Joshua Abreu/Asignacion 1/Practica1/src/main/java/com/mycompany/practica1/NumImparesFOR.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica1;
+
+/**
+ *
+ * @author Julio
+ */
+public class NumImparesFOR {
+    public static void main(String[] args) {
+        System.out.println("Numeros impares del 1 al 100");
+        
+        //va desde 1 a 100 y se suma de 2 en 2 dando los numeros impares hasta el 100
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
