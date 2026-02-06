c9a40545c8772789fe2e6dc6e7719aee83732310
gilbert14ot-ux <gilbert14ot@gmail.com>
2026-02-05 20:55:23 -0400

    Create Ejercicio_4.java

---
+++ 2025-2008 Gilber Vido/Asignacion 1/Ejercicio_4.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.teachbridge;
+
+/**
+ *
+ * @author gilber
+ */
+public class ejercicio4 {
+
+    /**
+     * @param args Este es el ejercicio #4 de la tarea
+     */
+    public static void main(String[] args) {
+         int i = 2;
+         
+        System.out.println("Numeros pares: ");
+        
+        while(i <= 100)
+        {
+            System.out.println(i);
+            i ++;
+        }
+    }
+    
+}
